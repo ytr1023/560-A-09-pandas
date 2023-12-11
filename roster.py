a312dc6b248449010bd7ca6c7d70171faedcaea9
ytr1023 <yetianrui1023@163.com>
2023-12-11 16:32:14 -0500

    Use pd instead of loop

--- roster.py
+++ roster.py
@@ -1,7 +1,7 @@
 #https://goheels.com/sports/mens-basketball/roster
-
+import pandas as pd
 roster = ["High", "Ryan", "Davis"]
-
-for player in roster:
-    print(player)
+player = {"Last Name": roster}
+data = pd.DataFrame(player)
+print(data)
 
