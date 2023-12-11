d0c1970ce9d14e3ba53eb09dcedf95bfc1546152
ytr1023 <yetianrui1023@163.com>
2023-12-11 16:58:36 -0500

    Update 10 players and round bmi

--- roster.py
+++ roster.py
@@ -1,13 +1,13 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
-player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando", "RJ", "Elliot"],
-          "height": [82, 72, 73],
-          "weight": [240, 180, 180],
+player = {"Last Name": ["High", "Cadeau", "Ryan", "Davis", "Bacot", "Trimble", "Wojcik", "Washington", "Lebo", "Landry"],
+          "First Name": ["Zayden", "Elliot", "Cormac", "RJ", "Armando", "Seth", "Paxson", "Jalen", "Creighton", "Rob"],
+          "height": [81, 73, 77, 72, 83, 75, 77, 82, 73, 76],
+          "weight": [225, 180, 195, 180, 240, 195, 195, 230, 180, 190],
           }
 data = pd.DataFrame(player)
 
-data["bmi"] = (data["weight"] / 2.205) / ((data["height"]/39.37)**2)
+data["bmi"] = (data["weight"] / (data["height"] ** 2) * 703).round(2)
 print(data)
 
 data.to_csv("bmi.csv")
